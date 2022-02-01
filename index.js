3329cf597fa1bc50908f400ad9b1bec3e451f83c
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-01 10:56:36 -0300

    crud

--- index.js
+++ index.js
@@ -13,7 +13,37 @@ server.get('/cursos/:index', (req, res) => {
     return res.json(cursos[index]);
 })
 
+// Retornar todos os cursos
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
 
+// Criar um novo curso
+server.post('/cursos', (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+})
+
+
+// Atualizar um curso
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+})
+
+// Deletar um curso
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    cursos.splice(index, 1);
+    return res.json({ message: 'O curso foi deletado' });
+})
 
 
 
